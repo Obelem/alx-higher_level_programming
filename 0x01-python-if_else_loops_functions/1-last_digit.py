b15161a1f6e837c5053b805823be7bd9aca5c0cb
Obelem <ibbraidejesse@gmail.com>
2022-06-28 17:32:40 +0100

    prints number with text to show if num > 5, equals 0 or < 6

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    buf = number * -1
+else:
+    buf = number
+
+lastDigit = buf % 10
+if number < 0:
+    lastDigit = lastDigit * -1
+
+if lastDigit == 0:
+    print(f"Last digit of {number:d} is 0 and is 0")
+
+elif lastDigit > 5:
+    print(f"Last digit of {number:d} is {lastDigit:d} and is greater than 5")
+
+elif lastDigit < 6:
+    print(f"Last digit of {number:d} is {lastDigit:d} and is less than 6 and not 0")
