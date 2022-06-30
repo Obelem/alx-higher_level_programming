142501cb0d15c11bec3db04a537cedb6665e8ce2
Obelem <ibbraidejesse@gmail.com>
2022-06-30 11:28:40 +0100

    does some basic maths

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+c = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, c))
+
+c = sub(a, b)
+print("{:d} - {:d} = {:d}".format(a, b, c))
+
+c = mul(a, b)
+print("{:d} * {:d} = {:d}".format(a, b, c))
+
+c = div(a, b)
+print("{:d} / {:d} = {:.0f}".format(a, b, c))
