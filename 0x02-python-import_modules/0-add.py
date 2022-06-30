16d837fbb865dff1f2feeae6070398fe7b546e52
Obelem <ibbraidejesse@gmail.com>
2022-06-30 12:41:57 +0100

    added if __name__ .. to prevent it from being run when imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,8 @@
 from add_0 import add
 
 if _name_ == "_main_":
+
     a = 1
     b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
