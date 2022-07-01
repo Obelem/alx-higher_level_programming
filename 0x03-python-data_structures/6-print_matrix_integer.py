2d1706c4e114d909b86165e5122439d97cfbaea9
Obelem <ibbraidejesse@gmail.com>
2022-07-01 21:39:28 +0100

    prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for item in row:
+            print("{:d}".format(item), end="" if item == row[-1] else " ") 
+        print()
