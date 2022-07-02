511251b616f3b8e74e9e789c477fe83ef7720213
Obelem <ibbraidejesse@gmail.com>
2022-07-02 13:33:37 +0100

    used to reverse() function to achieve this

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    items = len(my_list) - 1
-
-    for item in range(items, -1, -1):
-        print("{:d}".format(my_list[item]))
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
