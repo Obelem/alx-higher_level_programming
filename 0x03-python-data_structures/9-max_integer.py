40cad245aae03238f7c1bb568f4a733fef4cd705
Obelem <ibbraidejesse@gmail.com>
2022-07-02 21:28:58 +0100

    finds biggest int in list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_num = my_list[0]
+    for i in range(0, len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    return max_num
