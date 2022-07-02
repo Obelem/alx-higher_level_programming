8b54a5d93487e3f2e21141e7daf7aba57ad5c88b
Obelem <ibbraidejesse@gmail.com>
2022-07-02 21:05:00 +0100

    removed whitespace

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,10 +2,10 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence == "":
+    if length == 0:
         first_char = None
     else:
         first_char = sentence[0]
 
     new_tuple = (length, first_char)
-    return new_tuple 
+    return new_tuple
