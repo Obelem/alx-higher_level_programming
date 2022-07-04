dbce9f488ebda205ca13ba4d188ba738a09d326a
Obelem <ibbraidejesse@gmail.com>
2022-07-04 17:20:22 +0100

    checks if singly linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: head of singly linked list
+ * Return: 1 if palindrome else 0
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *temp = *head;
+	int head_val, tail_val;
+
+	/* get head value for comparison */
+	head_val = (*head)->n;
+
+	while (current)
+	{
+		/* look ahead to see if next node from current is the tail */
+		if ((current->next)->next == NULL)
+		{
+			/* assign val of tail for comparison */
+			tail_val = (current->next)->n;
+			/* reset tail to current */
+			current->next = NULL;
+
+			if (head_val != tail_val)
+				return (0);
+
+			/* update head (N/B: temp is serving as the head) */
+			temp = temp->next;
+			/* is a palindrome if head is NULL */
+			if (!temp)
+				return (1);
+
+			/* update head_val and current for traversing */
+			head_val = temp->n;
+			current = temp;
+		}
+		else
+			current = current->next;
+	}
+	return (0);
+}
