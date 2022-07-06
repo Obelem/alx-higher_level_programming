de4842a79442750893c60f9948fc689a52dcc133
Obelem <ibbraidejesse@gmail.com>
2022-07-06 11:35:27 +0100

    used sort and range to achieve this

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    my_list.sort()
+    add = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] != my_list[i - 1]:
+            add = add + my_list[i]
+    return add
