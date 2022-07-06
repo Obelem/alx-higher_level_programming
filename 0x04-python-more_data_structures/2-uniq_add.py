1c62f788434216c7c8c9ba24bf6e1d938a2cd7a8
Obelem <ibbraidejesse@gmail.com>
2022-07-06 12:42:57 +0100

    added single item condition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,9 @@
 def uniq_add(my_list=[]):
     my_list.sort()
     add = my_list[0]
+    if len(my_list) == 1:
+        return add
+
     for i in range(1, len(my_list)):
         if my_list[i] != my_list[i - 1]:
             add = add + my_list[i]
