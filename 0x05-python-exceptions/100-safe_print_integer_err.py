a5756429acdc7ce16d47878baf1fdf8a84ed0f77
Obelem <ibbraidejesse@gmail.com>
2022-07-18 15:47:24 +0100

    achieved using sys module

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: unkwown format code 'd'\
+    except ValueError:   
+        print("Exception: Unkwown format code 'd'\
  for object of type 'str'", file=sys.stderr)
         return False
