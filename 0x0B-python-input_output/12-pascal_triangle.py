e11ffd020e213eb90b21299322526d818eeeb3b3
Obelem <ibbraidejesse@gmail.com>
2022-08-02 23:32:40 +0100

    pascals triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""Defines a function that generates pascal's triangle"""
 
 
 def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal's triangle of n
+    Args:
+        n (int): size of triangle
+    """
     superList, current, next = [], [], []
 
     if n <= 0:
         return []
-    
+
     current.append(1)
     superList.append(current)
     for i in range(n - 1):
