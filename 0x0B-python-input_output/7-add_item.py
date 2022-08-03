07e8536829618889f967cd78db405b6081e30f59
Obelem <ibbraidejesse@gmail.com>
2022-08-03 01:21:24 +0100

    add args to python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+""" Module for task 7 """
 import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+FILENAME = "add_item.json"
+args = sys.argv
+args.pop(0)
+try:
+    items = load_from_json_file(FILENAME)
+except FileNotFoundError as e:
+    with open(FILENAME, "a") as f:
+        f.write('[]\n')
+    items = load_from_json_file(FILENAME)
+items.extend(args)
+save_to_json_file(items, FILENAME)
