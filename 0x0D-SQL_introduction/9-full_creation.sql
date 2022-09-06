4168a5bb783782392c1e93fc46e2ef1f78312126
Obelem <ibbraidejesse@gmail.com>
2022-09-06 20:17:00 +0100

    removed space b/w CREATE and INSERT

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 INSERT INTO `second_table` (id, name, score) VALUES
-(1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8);
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
