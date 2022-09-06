dff0d859ae2e2d9e06624eb5442dc4fd7313fbac
Obelem <ibbraidejesse@gmail.com>
2022-09-06 20:03:42 +0100

    inserted multiple rows using a single INSERT statement

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
-INSERT INTO `second_table` (id, name, score) VALUES
+INSERT INTO `second_table` (id, name, score) 
+VALUES
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
