d7929bc859567a01a343efcc206ad9092c8ffcac
Obelem <ibbraidejesse@gmail.com>
2022-09-06 17:42:04 +0100

    added a row into first_table using INSERT INTO

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in the table in my MySQL server
-INSERT INTO 'first_table' (id, name) VALUES (89, 'Best School')
+INSERT INTO 'first_table' ('id', 'name') VALUES (89, 'Best School')
