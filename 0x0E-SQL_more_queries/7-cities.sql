973b67ae209873f06d98c311d60fbd9ad44a00dc
Obelem <ibbraidejesse@gmail.com>
2022-09-07 20:35:45 +0100

    added database name to row

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
