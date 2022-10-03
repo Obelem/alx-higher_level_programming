6c7f8a27db8194aea32ebaa2a2a10fd5864d46cb
Jesse <ibbraidejesse@gmail.com>
2022-10-03 20:11:06 +0100

    removed variables

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const cText = 'C is fun';
-const pyText = 'Python is cool';
-const jsText = 'Javascript is amazing';
-console.log(cText);
-console.log(pyText);
-console.log(jsText);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
