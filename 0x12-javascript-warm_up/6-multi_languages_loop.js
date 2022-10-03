173b1a284aac494c8e710a5b0da15e315179e715
Jesse <ibbraidejesse@gmail.com>
2022-10-03 21:19:13 +0100

    removed spaces around square brackets

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const strArray = [
-  'C is fun', 'Python is cool', 'Javascript is amazing'
-];
+const strArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < strArray.length; i++) {
   console.log(strArray[i]);
