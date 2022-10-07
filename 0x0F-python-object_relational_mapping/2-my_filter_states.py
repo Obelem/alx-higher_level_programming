795a681d684215402c9695dc9922d5af6b919f8e
Jesse <ibbraidejesse@gmail.com>
2022-10-07 23:00:21 +0100

    stripped ' using strip()

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'\
-        ORDER BY id ASC".format(argv[4]))
+        ORDER BY id ASC".format(argv[4])).strip("'")
     [print(state) for state in cur.fetchall()]
