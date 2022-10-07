ddbc277e73b2bac0349bb4707621547f872542a0
Jesse <ibbraidejesse@gmail.com>
2022-10-07 22:48:53 +0100

    edited import of argv

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-import sys
-import MySQLdb
-
 """
     script that lists all states with a name starting
     with N (upper N) from the database hbtn_0e_0_usa
@@ -10,8 +6,11 @@ import MySQLdb
     ./1-filter_states.py <mysql username> <mysql password> <database name>
 """
 
+from sys import argv
+import MySQLdb
+
 if __name__ == "__main__":
-    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = con.cursor()
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in cur.fetchall() if state[1][0] == "N"]
