429a5eaf6a79bde8901723c1766d9b40eae6a5ee
Jesse <ibbraidejesse@gmail.com>
2022-10-07 22:51:58 +0100

    refactored code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,12 +4,12 @@ Displays all values in the states table of the database hbtn_0e_0_usa
 whose name matches that supplied as argument.
 Usage: ./2-my_filter_states.py <mysql usr> <mysql pwd> <db name> <state name>
 """
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = con.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY \
-        id ASC".format(sys.argv[4]))
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{:s}'\
+        ORDER BY id ASC".format(argv[4]))
     [print(state) for state in cur.fetchall()]
