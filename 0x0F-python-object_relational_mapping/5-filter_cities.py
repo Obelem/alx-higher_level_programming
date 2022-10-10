e852bb1c873c6cd9de9520828dbde8c8379e4f84
Jesse <ibbraidejesse@gmail.com>
2022-10-10 05:35:32 +0100

    used join() to print cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,8 @@ import MySQLdb
 if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities\
-                    JOIN states ON cities.state_id = state.id\
-                    WHERE states.name = argv[4]")
-    [print(city) for city in cur.fetchall()]
+    cur.execute("""SELECT * FROM cities INNER JOIN states
+                ON cities.state_id = states.id
+                ORDER BY cities.id""")
+    print(", ".join([city[2] for city in cur.fetchall()
+                     if city[4] == argv[4]]))
