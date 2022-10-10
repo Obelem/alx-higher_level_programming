8c7636d121b7b4b8afca7f7c3fb9deebddfe5cf7
Jesse <ibbraidejesse@gmail.com>
2022-10-10 05:46:28 +0100

    refactored code

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 """
-Defines the city model.
-Inherits from SQLAlchemy Base and links to the MySQL table states.
+class definition of a State and an instance Base = declarative_base()
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store cities.
-    id (sqlalchemy.Integer): The citiy's id.
-    name (sqlalchemy.String): The city's name.
     """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    City class
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 """
-Defines a State model.
-Inherits from SQLAlchemy Base and links to the MySQL table states.
+class definition of a State and an instance Base = declarative_base()
 """
+
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
     """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    State class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship('City', cascade='save-update, merge, delete',
+                          backref='state')
