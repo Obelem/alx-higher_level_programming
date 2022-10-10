835037a0edc19d7332dba803c3747779b4281da8
Jesse <ibbraidejesse@gmail.com>
2022-10-10 05:21:47 +0100

    refactored execute method

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,8 +9,7 @@ import MySQLdb
 if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT cities.name FROM cities
-                    JOIN states ON cities.state_id = state.id
-                    WHERE states.name = argv[4]
-                    """)
+    cur.execute("SELECT cities.name FROM cities\
+                    JOIN states ON cities.state_id = state.id\
+                    WHERE states.name = argv[4]")
     [print(city) for city in cur.fetchall()]
