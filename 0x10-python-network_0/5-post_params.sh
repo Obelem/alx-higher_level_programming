2054040fecb3347fabda8bc94999e05c15457b75
Jesse <ibbraidejesse@gmail.com>
2022-10-20 15:24:30 +0100

    first POST request

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# sends a POST request to the passed URL, and displays the body of the response
+email="test@gmail.com"
+sub="I will always be here for PLD"
+curl -s "$1" -X POST -d "email=$email&subject=$sub"
