27eea333648b5d20e8f9148c42f2f9397a297bde
Jesse <ibbraidejesse@gmail.com>
2022-10-20 11:54:32 +0100

    modified curl arguments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL and displays size of body response
-curl -sI 0.0.0.0:5000 | grep -i Content-Length | cut -d " " -f2
+# display number of bytes in location
+curl -s "$1" | wc -c
