c239eb934a732553a9c7a69799ca938a61e41d89
Jesse <ibbraidejesse@gmail.com>
2022-10-20 15:26:00 +0100

    modified curl arguments

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,4 @@
 # sends a POST request to the passed URL, and displays the body of the response
 email="test@gmail.com"
 sub="I will always be here for PLD"
-curl -s "$1" -X POST -d "email=$email&subject=$sub"
+curl -s -X POST -d "email=$email&subject=$sub" "$1"
