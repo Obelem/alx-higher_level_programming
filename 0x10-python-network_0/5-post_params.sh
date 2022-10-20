c947ff2f69b5c0380adb0259693bfc9eff53dbaa
Jesse <ibbraidejesse@gmail.com>
2022-10-20 15:28:23 +0100

    removed shell variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response
-email="test@gmail.com"
-sub="I will always be here for PLD"
-curl -s -X POST -d "email=$email&subject=$sub" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
