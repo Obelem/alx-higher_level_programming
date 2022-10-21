b1968fa471199067a71b6680bd9f881fdb212844
Jesse <ibbraidejesse@gmail.com>
2022-10-21 15:59:12 +0100

    made executable

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,6 @@
+""" displays the value of the variable X-Request-Id in the response header """
+from sys import argv
+from requests import get
+
+if __name__ == "__main__":
+    print(get(argv[1]).headers.get('X-Request-Id'))
