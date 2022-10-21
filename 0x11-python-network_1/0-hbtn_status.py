b8cfc51c0f7816a74e263c660bd6756182fc9b5d
Jesse <ibbraidejesse@gmail.com>
2022-10-21 13:06:45 +0100

    removed f strings

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # use urllib to fetch given url with formatted display
 
-if __name__ == "__main__":
-    import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    with urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print('Body reponse:')
         print('\t- type:', type(content))
         print('\t- content:', content)
