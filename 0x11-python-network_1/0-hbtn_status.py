f2d639c40c8aa84cfe5be5f681e449748a9c86be
Jesse <ibbraidejesse@gmail.com>
2022-10-21 12:56:34 +0100

    first user of urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# use urllib to fetch given url with formatted display
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body reponse:')
+        print('\t- type:', type(content))
+        print('\t- content:', content)
+        print('\t- utf8 content:', content.decode('utf-8'))
