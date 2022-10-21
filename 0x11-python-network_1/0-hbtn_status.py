7d4ae10cd3d7ca2bef9db08ef81ede1b4867ce3b
Jesse <ibbraidejesse@gmail.com>
2022-10-21 14:13:44 +0100

    modified import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """use urllib to fetch given url with formatted display"""
+from urllib.request import urlopen
 
 if __name__ == '__main__':
-    from urllib.request import urlopen
-
     with urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print('Body reponse:')
