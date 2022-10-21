deeee62518b49477be98c815a5efbed53604a418
Jesse <ibbraidejesse@gmail.com>
2022-10-21 13:14:39 +0100

    changed permissions

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@
 if __name__ == "__main__":
     from urllib.request import urlopen
 
-    with urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print('Body reponse:')
         print('\t- type:', type(content))
         print('\t- content:', content)
