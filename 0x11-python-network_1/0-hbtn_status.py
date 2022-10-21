f48fa48c220b051593054db23c140e3bb7b538c8
Jesse <ibbraidejesse@gmail.com>
2022-10-21 15:35:46 +0100

    edited py docs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
+"""  fetches https://alx-intranet.hbtn.io/status """
 from urllib.request import urlopen
 
 if __name__ == '__main__':
