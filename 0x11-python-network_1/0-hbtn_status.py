83b594d3505fefd3f657cce8ee4ce3edfefe6aa7
Jesse <ibbraidejesse@gmail.com>
2022-10-21 13:03:34 +0100

    edited import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 # use urllib to fetch given url with formatted display
 
 if __name__ == "__main__":
-    from urllib.request import urlopen
+    import urllib.request
 
-    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print('Body reponse:')
-        print(f'\t- type: {type(content)}')
-        print(f'\t- content: {content}')
-        print(f'\t- utf8 content: {content.decode("utf-8")}')
+        print('\t- type:', type(content))
+        print('\t- content:', content)
+        print('\t- utf8 content:', content.decode('utf-8'))
