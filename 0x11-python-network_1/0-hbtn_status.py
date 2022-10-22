fa7aa0fd489638fa9e066e334e174eecbd754b1e
Jesse <ibbraidejesse@gmail.com>
2022-10-22 05:38:13 +0100

    used f strings

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
         print('Body reponse:')
         print(f'\t- type: {type(content)}')
         print(f'\t- content: {content}')
-        print(f'\t- utf8 content: {content.decode('utf-8')}')
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
