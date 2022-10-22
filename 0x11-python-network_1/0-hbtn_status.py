179cbd0877d06b48441350c4464988d113aa0bb8
Jesse <ibbraidejesse@gmail.com>
2022-10-22 05:35:46 +0100

    refactored code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,10 @@
 
 if __name__ == '__main__':
     from urllib.request import urlopen
-    from urllib.request import Request
 
-    req = Request('https://alx-intranet.hbtn.io/status')
-
-    with urlopen(req) as res:
+    with urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
-    print('Body reponse:')
-    print('\t- type:', type(content))
-    print('\t- content:', content)
-    print('\t- utf8 content:', content.decode('utf-8'))
+        print("Body reponse:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
