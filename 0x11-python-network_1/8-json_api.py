4f0047e52e3c959f66486cdb65e945f8be387d90
Jesse <ibbraidejesse@gmail.com>
2022-10-22 04:37:13 +0100

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" first implementation of API's """
+
+if __name__ == '__main__':
+    from sys import argv
+    from requests import post
+
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = '' if len(argv) == 1 else argv[1]
+    res = post(url, data={'q': letter})
+
+    try:
+        res_json = res_dict.json()
+        if len(res_json) == 0:
+            print('No result')
+        else:
+            print(f'[{res_json.get("id")}] {res_json.get("name")}')
+    except:
+        print('Not a valid JSON')
