bb3b4a12551cec3d639852ad3d22b88af223d3b5
Jesse <ibbraidejesse@gmail.com>
2022-10-22 05:42:00 +0100

    used f strings

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """  fetches https://alx-intranet.hbtn.io/status """
 
-if __name__ == '__main__':
-    from urllib.request import urlopen
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
 
-    with urlopen('https://alx-intranet.hbtn.io/status') as res:
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as res:
         content = res.read()
-    print('Body reponse:')
-    print(f'\t- type: {type(content)}')
-    print(f'\t- content: {content}')
-    print(f'\t- utf8 content: {content.decode("utf-8")}')
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
