9917e8c1ba47a9c1aee59d9f63faa25a612be3b5
Jesse <ibbraidejesse@gmail.com>
2022-10-22 03:49:16 +0100

    changed print context

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     with urlopen(req) as res:
         content = res.read()
-        print('Body reponse:')
-        print('\t- type:', type(content))
-        print('\t- content:', content)
-        print('\t- utf8 content:', content.decode('utf-8'))
+    print('Body reponse:')
+    print('\t- type:', type(content))
+    print('\t- content:', content)
+    print('\t- utf8 content:', content.decode('utf-8'))
