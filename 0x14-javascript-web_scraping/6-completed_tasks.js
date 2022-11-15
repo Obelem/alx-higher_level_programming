57775706d3e469c5772d5a024440f2d9ec44777f
Jesse <ibbraidejesse@gmail.com>
2022-11-15 13:20:28 +0100

    refactored code

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,17 +2,15 @@
 const request = require('request');
 const endpoint = process.argv[2];
 const obj = {};
-let testId = 0;
 
 request(endpoint, (err, res, body) => {
   if (err) throw err;
 
   JSON.parse(body).forEach(todo => {
-    if (todo.completed === true && testId === todo.userId) {
-      if (obj[todo.userId] === undefined) obj[todo.userId] = 0;
-      obj[todo.userId]++;
-    } else if (todo.completed) obj[todo.userId] = 1;
-    testId = todo.userId;
+    const id = todo.userId;
+    if (!obj[id]) obj[id] = 0;
+    if (todo.completed) obj[id]++;
+    if (obj[id] === 0) delete obj[id];
   });
   console.log(obj);
 });
